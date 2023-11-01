22851fb787d3cf0c94ede99c324da9248fdb4801
axys-creative <axyscreativeagency@gmail.com>
2023-10-31 22:17:45 -0600

    tooltip added for membership

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,3 @@
 import { toggleNav } from "./nav.js";
-// import { gsapAnimations } from "./animations.js";
+import { gsapAnimations } from "./animations.js";
 import { questions } from "./questions.js";
